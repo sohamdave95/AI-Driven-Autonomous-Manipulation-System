fdfc6728ebb4834851199ff19d733cae97c96ec6
sohamdave95 <davesoh929@gmail.com>
2026-08-05 14:04:12 -0400

    Fix IK math and add servo/elevation offsets
    Correct inverse kinematics calculations and introduce servo offsets. Changes include: set zAxisOffset to -7.2 and add baseServoOffset; add elevationAngle variable; change findAngle to return double; apply zAxisOffset with addition; fix totalLength to use sqrt(planarLength^2 + z^2) instead of incorrect expression; compute elevationAngle and incorporate it into hip and knee angle calculations; apply baseServoOffset to baseAngle and constrain all angles; update test call to calculateIK(5, 5, 3). These edits fix angle computations and adjust servo centering for expected physical behavior.

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,10 @@ Servo base;
 Servo hip;
 Servo knee; 
 
-const double xAxisOffset = 10.3;
-const double zAxisOffset = 3.2;
-const double yAxisOffset = 0;
-
+const double xAxisOffset = 10.3; // 10.3
+const double zAxisOffset = -7.2;
+const double yAxisOffset = 0; // 0
+const int baseServoOffset = 90;
 const double hipLength = 12.3;
 const double kneeLength = 12;
 const int reachLimit = hipLength + kneeLength;
@@ -19,6 +19,7 @@ double totalLength;
 double baseAngle;
 double hipAngle;
 double kneeAngle;
+double elevationAngle;
 
 void setup() {
   Serial.begin(115200);
@@ -31,24 +32,24 @@ void setup() {
   
 }
 
-int findAngle(double OppositeSide, double otherSideA, double otherSideB){
+double findAngle(double OppositeSide, double otherSideA, double otherSideB){
   return constrain(degrees(acos(((OppositeSide*OppositeSide) - (otherSideA*otherSideA) - (otherSideB*otherSideB))/(-2*otherSideA*otherSideB))), 0, 180);
 }
 
 void calculateIK(double x, double y, double z){
     x += xAxisOffset;
     y += yAxisOffset;
-    z -= zAxisOffset;
+    z += zAxisOffset;
 
     planarLength = sqrt(x*x + y*y);
-    totalLength = sqrt(planarLength + z);
+    totalLength = sqrt(planarLength*planarLength + z*z);
     
     if (totalLength <= reachLimit){
 
-  
-    baseAngle = constrain(degrees(atan2(y,x)) + 90, 0, 180);
-    hipAngle = findAngle(kneeLength, hipLength, totalLength);
-    kneeAngle = findAngle(totalLength, kneeLength, hipLength) + hipAngle;
+    elevationAngle = constrain(degrees(atan2(z , planarLength)), 0, 180);
+    baseAngle = constrain(degrees(atan2(y,x)) + baseServoOffset, 0, 180);
+    hipAngle = constrain(findAngle(kneeLength, hipLength, totalLength) + elevationAngle, 0, 180);
+    kneeAngle = constrain(findAngle(totalLength, kneeLength, hipLength) + hipAngle, 0, 180);
 
     base.write(baseAngle);
     hip.write(hipAngle);
@@ -68,7 +69,7 @@ void calculateIK(double x, double y, double z){
 }
 
 void loop() {
-  calculateIK(0,10,0);
+  calculateIK(5, 5, 3);
 }
 
 
