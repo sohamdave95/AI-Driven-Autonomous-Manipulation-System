075f4c73d7f268f51fcfd7a5fe80c3cb2fbf1630
sohamdave95 <davesoh929@gmail.com>
2026-08-07 00:20:18 -0400

    Fix IK limits and offset bounds

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@ const int baseServoOffset = 90;
 const double hipLength = 12.3;
 const double kneeLength = 12;
 const int reachLimit = hipLength + kneeLength;
+const int minLength = 8;
 double planarLength;
 double totalLength;
 
@@ -44,12 +45,12 @@ void calculateIK(double x, double y, double z){
     planarLength = sqrt(x*x + y*y);
     totalLength = sqrt(planarLength*planarLength + z*z);
     
-    if (totalLength <= reachLimit){
+    if (totalLength <= reachLimit && planarLength >= minLength){
 
     elevationAngle = degrees(atan2(z , planarLength));
     baseAngle = constrain(degrees(atan2(y,x)) + baseServoOffset, 0, 180);
     hipAngle = constrain(findAngle(kneeLength, hipLength, totalLength) + elevationAngle, 0, 180);
-    kneeAngle = constrain(90 + (180 - findAngle(totalLength, kneeLength, hipLength)), 0, 180);
+    kneeAngle = constrain((180 - findAngle(totalLength, kneeLength, hipLength)), 0, 180);
     /* explain Knee Angle:
       180 - findAngle gets you the external angle of the joint (how much servo must rotate)
       that has to be added from the straight line of the arm, so from 90 deg which is the angle when the arm is straight
@@ -70,16 +71,15 @@ void calculateIK(double x, double y, double z){
 
     }
     else{
-      Serial.println("Target too far.");
+      Serial.println("Target too far or close.");
       delay(1000);
     }
 
 }
 
 void loop() {
-  calculateIK(10, 0, 10);
+  calculateIK(0, 5, 5);
 }
-
 
 
 
