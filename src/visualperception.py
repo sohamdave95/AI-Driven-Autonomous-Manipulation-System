b0d35333125b1ab3fd17969793677ab22b7b5dc5
sohamdave95 <davesoh929@gmail.com>
2026-09-01 22:19:27 -0400

    working object classification, wip

--- src/visualperception.py
+++ src/visualperception.py
@@ -1,12 +1,23 @@
 import cv2
 import numpy as np
+import torch
+from transformers import OwlViTProcessor, OwlViTForObjectDetection
+from PIL import Image
+
+
+processor = OwlViTProcessor.from_pretrained("google/owlvit-base-patch32")
+model = OwlViTForObjectDetection.from_pretrained("google/owlvit-base-patch32")
+model.eval()
 
 camera = cv2.VideoCapture(0)
 
 while True:
     ret, frame = camera.read()
 
-
+    if not ret:
+        print("Failed to grab frame")
+        break
+    
     frame = cv2.flip(frame, 1)
 
     height, width, _ = frame.shape
@@ -29,13 +40,43 @@ while True:
 
     imgMatrix = cv2.getPerspectiveTransform(ptLocations, transformedPoints)
     transformedImg = cv2.warpPerspective(frame, imgMatrix, (640, 480))
+
+    texts = [["toy car", "Hot Wheels car"]]
+
+    
+    image_rgb = cv2.cvtColor(transformedImg, cv2.COLOR_BGR2RGB)
+    pil_image = Image.fromarray(image_rgb)
+
+    
+    inputs = processor(text=texts, images=pil_image, return_tensors="pt")
+    with torch.no_grad():
+        outputs = model(**inputs)
+
+    
+    target_sizes = torch.tensor([pil_image.size[::-1]])
+    results = processor.image_processor.post_process_object_detection(outputs, threshold=0.15, target_sizes=target_sizes)[0]
+    for box, score, label in zip(results["boxes"], results["scores"], results["labels"]):
+        box = [int(i) for i in box.tolist()]
+        x1, y1, x2, y2 = box
+
+        u = int((x1 + x2) / 2)
+        v = int((y1 + y2) / 2)
+
+        cv2.rectangle(transformedImg, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.circle(transformedImg, (u, v), 5, (0, 0, 255), -1)
+
+    
+    
+
+
+
+
+
     #simple perspective transform wip.
     cv2.imshow("webcam", frame)
     cv2.imshow("bird's eye", transformedImg)
 
-    if not ret:
-        print("Failed to grab frame")
-        break
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 camera.release()
