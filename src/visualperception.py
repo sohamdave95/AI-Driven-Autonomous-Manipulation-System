8a3754d601e43f6d0e0be3779cd69a913f8068f8
sohamdave95 <davesoh929@gmail.com>
2026-09-05 09:15:48 -0400

    switch to yolo model for easier integration vs huggingface

--- src/visualperception.py
+++ src/visualperception.py
@@ -1,13 +1,11 @@
 import cv2
 import numpy as np
 import torch
-from transformers import OwlViTProcessor, OwlViTForObjectDetection
-from PIL import Image
+from ultralytics import YOLO
 
 
-processor = OwlViTProcessor.from_pretrained("google/owlvit-base-patch32")
-model = OwlViTForObjectDetection.from_pretrained("google/owlvit-base-patch32")
-model.eval()
+model = YOLO("yolov8s-world.pt")
+model.set_classes(["toy car", "pen", "earbuds", "hand", "rocket nosecone"])
 
 camera = cv2.VideoCapture(0)
 
@@ -41,41 +39,15 @@ while True:
     imgMatrix = cv2.getPerspectiveTransform(ptLocations, transformedPoints)
     transformedImg = cv2.warpPerspective(frame, imgMatrix, (640, 480))
 
-    texts = ["toy car", "Hot Wheels car"]
-
-    
-    image_rgb = cv2.cvtColor(transformedImg, cv2.COLOR_BGR2RGB) #learned this the hard way, converts color channels from bgr as in cv2 to rgb for the huggingface model.
-    pil_image = Image.fromarray(image_rgb)
-
-    
-    inputs = processor(text=texts, images=pil_image, return_tensors="pt") #feed images into model as a tensor
-    with torch.no_grad():
-        outputs = model(**inputs)
-
-    
-    target_sizes = torch.tensor([pil_image.size[::-1]])
-    results = processor.image_processor.post_process_object_detection(outputs, threshold=0.15, target_sizes=target_sizes)[0]
-    for box, score, label in zip(results["boxes"], results["scores"], results["labels"]):
-        box = [int(i) for i in box.tolist()]
-        x1, y1, x2, y2 = box
-
-        u = int((x1 + x2) / 2)
-        v = int((y1 + y2) / 2)
-
-        cv2.rectangle(transformedImg, (x1, y1), (x2, y2), (0, 255, 0), 2)
-        cv2.circle(transformedImg, (u, v), 5, (0, 0, 255), -1)
-
-    
-    
-
-
-
-
-
     #simple perspective transform wip.
-    cv2.imshow("webcam", frame)
-    cv2.imshow("bird's eye", transformedImg)
+    #cv2.imshow("webcam", frame)
+    
 
+    results = model(transformedImg, conf=0.25)
+    annotated_frame = results[0].plot()
+
+    cv2.imshow("bird's eye", transformedImg)
+    cv2.imshow("objectdetection", annotated_frame)
     
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
